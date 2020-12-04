732d4e9f7db8cfaa07d2272482a3695aa1db5171
ceford <cliff@ford.myzen.co.uk>
2020-12-04 11:39:49 +0000

    Mostly copy of com_media

---
+++ com_mediacat/admin/layouts/toolbar/create-folder.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_mediacat
+ *
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+Factory::getDocument()->getWebAssetManager()
+	->useScript('webcomponent.toolbar-button');
+
+$title = Text::_('COM_MEDIACAT_CREATE_NEW_FOLDER');
+?>
+<joomla-toolbar-button>
+	<button class="btn btn-info" onclick="MediacatManager.Event.fire('onClickCreateFolder');">
+		<span class="icon-folder icon-fw" aria-hidden="true"></span>
+		<?php echo $title; ?>
+	</button>
+</joomla-toolbar-button>

4e6f6af323911b8735f32c025fcf1438991fbdae
ceford <cliff@ford.myzen.co.uk>
2020-12-31 10:29:47 +0000

    Updated Help

--- com_mediacat/admin/tmpl/files/default.php
+++ com_mediacat/admin/tmpl/files/default.php
@@ -54,7 +54,7 @@ $listDirn   = $this->escape($this->state->get('list.direction'));
 					<?php if (empty($this->items)) : ?>
 					<div class="alert alert-info">
 						<span class="icon-info-circle" aria-hidden="true"></span><span class="sr-only"><?php echo Text::_('INFO'); ?></span>
-						<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS') . Text::_('COM_MEDIACAT_SELECT_FOLDERS_INDEXER'); ?>
+						<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
 					</div>
 					<?php else : ?>
 					<table class="table" id="articleList">
